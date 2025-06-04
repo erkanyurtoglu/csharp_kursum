306d8ba805a59c01c5b147432c6a15a3bed92ac7
erkanyurtoglu <158731690+erkanyurtoglu@users.noreply.github.com>
2025-06-04 19:58:38 +0300

    Label ve textbox öğrenildi.

--- temelAracKullanimlari/labelVeTextBox/labelVeTextBox/Form1.cs
+++ temelAracKullanimlari/labelVeTextBox/labelVeTextBox/Form1.cs
@@ -16,5 +16,17 @@ namespace labelVeTextBox
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label2.Text = "Erkan YURTOĞLU";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // textBox1.Text = "Yazılım Geliştirici";
+            label3.Text = textBox1.Text;
+            textBox1.Clear();
+        }
     }
 }
